290fba83cc463dad7f7cb6e40665d6130c0b1b4e
Elaine Soares <elaine.fabiola@gmail.com>
2025-08-10 23:25:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos= [];
+
+function adicionarAmigo() {
+  let amigo = document.querySelector('input').value.trim();
+  if (amigo === "") {
+    alert("Por favor, digite o nome do amigo.");
+    return;
+  }
+
+  listaAmigos.push(amigo); 
+  console.log(listaAmigos);
+  let exibirNome = document.getElementById('listaAmigos');
+      exibirNome.innerHTML = listaAmigos.join('<br>');
+    
+}
+function sortearAmigo(){
+    let numeroLista = listaAmigos.length;
+    let amigoEscolhido = parseInt(Math.random() * numeroLista +1);
+     let amigoSorteado = listaAmigos[amigoEscolhido];
+      document.getElementById('resultado').innerHTML = amigoSorteado;
+}
